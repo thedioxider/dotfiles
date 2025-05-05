d5846b7965cdff2e1011122631a4ef3eb004b89f
Demetrius <thedioxider@mail.ru>
2025-05-06 01:41:44 +0300

    feat(alias): plz for sudo (hi, Asquar); ew for trash

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,8 +6,12 @@ alias cp='cp -i'
 alias mv='mv -i'
 
 if status is-interactive
+    alias plz=sudo
+
     # Commands to run in interactive sessions can go here
     alias cz=chezmoi
     # file manager
     alias fm=yazi
+    # trash manager
+    alias ew=trash put
 end
